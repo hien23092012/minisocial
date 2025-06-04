e7e01c1f4b586cdeafd705a12416d6e998dd7e00
hien23092012 <122507922+hien23092012@users.noreply.github.com>
2025-06-04 16:31:22 +0700

    Update script.js

--- mini_social_project/static/js/script.js
+++ mini_social_project/static/js/script.js
@@ -1,1 +1,21 @@
+// static/js/script.js
+const apiUrl = 'http://localhost:5000/api';
 
+function escapeHTML(str) {
+    // ... (code của hàm escapeHTML)
+}
+
+function getInitials(name) {
+    // ... (code của hàm getInitials)
+}
+
+async function fetchPosts() {
+    // ... (code của hàm fetchPosts)
+}
+
+function renderPosts(posts) {
+    // ... (code của hàm renderPosts)
+}
+// ... (toàn bộ code JavaScript còn lại từ thẻ <script> trong index.html) ...
+
+window.onload = fetchPosts;
